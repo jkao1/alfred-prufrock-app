c98c7991db28f6c2b090186cc4aba548c8286b3e
Jacob Beauchamp <jacobeau@indiana.edu>
2017-09-24 03:58:48 -0400

    im tired

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 9000);
 app.use(compression());
 app.use(logger("dev"));
 app.use(bodyParser.json());
